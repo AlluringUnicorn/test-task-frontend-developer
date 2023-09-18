ac348a0782e377d3e85fda5730d23d2b1b153873
Mariia Khapun <111394270+AlluringUnicorn@users.noreply.github.com>
2023-09-18 23:32:17 +0300

    added/styles folder

--- frontend/src/strapi.js
+++ frontend/src/strapi.js
@@ -1,9 +1,6 @@
 export async function fetchApi({ endpoint, wrappedByKey }) {
-  if (endpoint.startsWith("/")) {
-    endpoint = endpoint.slice(1);
-  }
 
-  const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}?populate=*`);
+  const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
 
   const res = await fetch(url.toString());
   let data = await res.json();
