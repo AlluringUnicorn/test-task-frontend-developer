f7e1d7013341038864b404ab8ef0fad3a8604510
Mariia Khapun <111394270+AlluringUnicorn@users.noreply.github.com>
2023-09-19 11:07:02 +0300

    fix/bugs

--- frontend/src/strapi.js
+++ frontend/src/strapi.js
@@ -1,6 +1,5 @@
 export async function fetchApi({ endpoint, wrappedByKey }) {
-
-  const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
+  const url = new URL(`http://127.0.0.1:1337/api/${endpoint}`);
 
   const res = await fetch(url.toString());
   let data = await res.json();
